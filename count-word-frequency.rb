1ba63eed66f74bb8b953112e875480197c498396
Steve Cohen <scohen@scohen.org>
2015-02-23 10:54:42 -0800

    Used block, closed file
    I used the block file.open, which will close your file for you and changed the Hash constructor to use the block-oriented one, which simplifies the code quite a bit.

--- count-word-frequency.rb
+++ count-word-frequency.rb
@@ -6,18 +6,12 @@ job_posting = "job-posting.txt"
 
 #This method returns 250 most frequent words from a text file
 def count_word_frequency(textfile)
-  f = File.open(textfile, "r")
-	word_count_hash = {}
-		f.each_line do |line|
-		  line.split(" ").each do |word|
-		  	word = word.downcase.gsub(/[^a-z\s]/, '')
-		  	if  word_count_hash.has_key?(word)
-			  	word_count_hash[word] += 1
-				else
-			  	word_count_hash[word] = 1
-			  end 
-			end 
-		end
+  word_count_hash = Hash.new{|h,k| h[k] = 0}
+  File.open(textfile, "r") do |file|
+    file.each_line do |line|
+      line.split(" ").each{|word| word_count_hash[word] += 1}
+    end	
+  end
   top_250_words = Hash[word_count_hash.sort_by {|k,v| v}.reverse.first(250)]
   puts top_250_words
 end 
